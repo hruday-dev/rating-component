09c120b0e0ea1049f5f0a956dfac81a14ca05861
hruday-dev <thirumalahrudayboda@gmail.com>
2025-11-11 14:01:10 +0530

    updated rating component structure and styles implement feedback form and thank you message sections

---
+++ app.js
@@ -0,0 +1,21 @@
+const rating = document.querySelectorAll('a');
+const submit =document.querySelector('button');
+const feedback = document.getElementById('feedback-form');
+const thankMessage = document.getElementById('thanks-message');
+const displayrating = document.querySelector('span');
+let rated = 0;
+submit.addEventListener('click',(e)=>{
+    e.preventDefault();
+    feedback.style.display = 'none';
+    thankMessage.style.display ='flex';
+    displayrating.textContent = `You selected ${rated} out of 5`
+})
+
+for(let ratebtn of rating){
+    ratebtn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        rating.forEach((r)=>r.classList.remove('clicked'));
+        ratebtn.classList.add('clicked');
+        rated = ratebtn.textContent;
+    })
+}
